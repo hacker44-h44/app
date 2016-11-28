f30bb141d7984fa544006d655b634f12f7ef44c2
hacker44 <hacker44-h44@users.noreply.github.com>
2016-11-28 10:41:37 +0330

    Update private.lua

--- plugins/private.lua
+++ plugins/private.lua
@@ -5,8 +5,8 @@ local function do_keyboard_credits()
     keyboard.inline_keyboard = {
     	{
     		{text = _("Channel"), url = 'https://telegram.me/'..config.channel:gsub('@', '')},
-    		{text = _("GitHub"), url = 'https://github.com/RememberTheAir/GroupButler'},
-    		{text = _("Rate me!"), url = 'https://telegram.me/storebot?start='..bot.username},
+    		{text = _("hacker44"), url = 'https://telegram.me/hacker44'},
+    		{text = _("mr.pink"), url = 'https://telegram.me/rahbar_salibam},
 		},
 		{
 			{text = _("👥 Groups"), callback_data = 'private:groups'}
@@ -32,7 +32,7 @@ function plugin.onTextMessage(msg, blocks)
 	end
 	if blocks[1] == 'about' then
 		local keyboard = do_keyboard_credits()
-		local text = _("This bot is based on [otouto](https://github.com/topkecleon/otouto) (AKA @mokubot, channel: @otouto), a multipurpose Lua bot.\nGroup Butler wouldn't exist without it.\n\nThe owner of this bot is @bac0nnn, do not pm him: use /groups command instead.\n\nBot version: `%s`\n*Some useful links:*"):format(config.human_readable_version .. ' rev.' .. bot.revision)
+		local text = _("This bot is created by [BHH team](https://telegram.me/Bhh_ch) (hacker44 & mr.pink), a multipurpose Lua bot.\n\nThe owner of this bot is @hacker44 & @rahbar_salibam,  use /groups command\n\nBot version: `%s`\n*Some useful links:*"):format(config.human_readable_version .. ' rev.' .. bot.revision)
 		api.sendMessage(msg.chat.id, text, true, keyboard)
 	end
 	if blocks[1] == 'groups' then
@@ -54,7 +54,7 @@ end
 function plugin.onCallbackQuery(msg, blocks)
 	if blocks[1] == 'about' then
 		local keyboard = do_keyboard_credits()
-		local text = _("This bot is based on [otouto](https://github.com/topkecleon/otouto) (AKA @mokubot, channel: @otouto), a multipurpose Lua bot.\nGroup Butler wouldn't exist without it.\n\nThe owner of this bot is @bac0nnn, do not pm him: use /groups command instead.\n\nBot version: `%s`\n*Some useful links:*"):format(config.human_readable_version .. ' rev.' .. bot.revision)
+		local text = _("This bot is created by [BHH team](https://telegram.me/Bhh_ch) (hacker44 & mr.pink), a multipurpose Lua bot.\n\nThe owner of this bot is @hacker44 & @rahbar_salibam,  use /groups command\n\nBot version: `%s`\n*Some useful links:*"):format(config.human_readable_version .. ' rev.' .. bot.revision)
 		api.editMessageText(msg.chat.id, msg.message_id, text, true, keyboard)
 	end
 	if blocks[1] == 'groups' then
